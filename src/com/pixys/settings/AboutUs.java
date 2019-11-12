PixysOS/packages_apps_Settings

Settings: About PixysOS Bring UP Credits: akhilnarang KreAch3R Adarsh-MR
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

src/com/android/settings/AboutUs.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2018 PixysOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+import android.os.Bundle;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class AboutUs extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.about_us);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.PIXYS;
+    }
+
+}

PixysOS/packages_apps_PixysSettings

Change-Id: I41055466fd7df9211bea9060610c03c4138bf051
Signed-off-by: Subinsmani <subins.mani@gmail.com>

src/com/pixys/settings/AboutUs.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2018 PixysOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixys.settings;
+
+import android.os.Bundle;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class AboutUs extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.about_us);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.PIXYS;
+    }
+
+}