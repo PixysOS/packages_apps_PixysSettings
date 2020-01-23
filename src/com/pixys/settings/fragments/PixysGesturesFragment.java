b41c30696db90ad1e9e76ef78cddb1de97431169
Adin Kwok <adin.kwok@carbonrom.org>
2020-01-23 17:17:28 -0500

    Move gestures into PixysSettings [2/2]
    Change-Id: Id4c7169dd8939d759c67ebc7afa255ef6e4d8323
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Psycho Mods <nishsin26@gmail.com>

--- src/com/pixys/settings/fragments/PixysGesturesFragment.java
+++ src/com/pixys/settings/fragments/PixysGesturesFragment.java
@@ -17,31 +17,88 @@
 
 package com.pixys.settings.fragments;
 
+import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.gestures.GestureSettings;
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.Utils;
 
-public class PixysGesturesFragment extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class PixysGesturesFragment extends GestureSettings implements Indexable {
+
+    private static final String TAG = "Gestures";
+
+    private static final String KEY_ASSIST = "gesture_assist_input_summary";
+    private static final String KEY_SWIPE_DOWN = "gesture_swipe_down_fingerprint_input_summary";
+    private static final String KEY_DOUBLE_TAP_POWER = "gesture_double_tap_power_input_summary";
+    private static final String KEY_DOUBLE_TWIST = "gesture_double_twist_input_summary";
+    private static final String KEY_NAVIGATION_INPUT = "gesture_system_navigation_input_summary";
+    private static final String KEY_TAP_SCREEN = "gesture_tap_screen_input_summary";
+    private static final String KEY_DOUBLE_TAP_SCREEN = "gesture_double_tap_screen_input_summary";
+    private static final String KEY_PICK_UP = "gesture_pick_up_input_summary";
+    private static final String KEY_PREVENT_RINGING = "gesture_prevent_ringing_summary";
+    private static final String KEY_GESTURE_GLOBAL_ACTIONS_PANEL = "gesture_global_actions_panel_summary";
+
+    private ContentResolver mContentResolver;
+    private SwitchPreference mDoubleTapStatusBarToSleep;
+    private SwitchPreference mDoubleTapLockScreenToSleep;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.pixys_settings_gestures);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        super.onCreatePreferences(savedInstanceState, rootKey);
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.PIXYS;
+        return MetricsProto.MetricsEvent.PIXYS;
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return true;
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER = new BaseSearchIndexProvider() {
+
+        @Override
+        public List<SearchIndexableResource> getXmlResourcesToIndex(Context context, boolean enabled) {
+            ArrayList<SearchIndexableResource> result = new ArrayList<SearchIndexableResource>();
+            SearchIndexableResource searchIndexableResource = new SearchIndexableResource(context);
+            searchIndexableResource.xmlResId = R.xml.pixys_settings_gestures;
+            result.add(searchIndexableResource);
+            return result;
+        }
+
+        @Override
+        public List<String> getNonIndexableKeys(Context context) {
+            List<String> keys = super.getNonIndexableKeys(context);
+                    keys.add(KEY_ASSIST);
+                    keys.add(KEY_SWIPE_DOWN);
+                    keys.add(KEY_DOUBLE_TAP_POWER);
+                    keys.add(KEY_DOUBLE_TWIST);
+                    keys.add(KEY_NAVIGATION_INPUT);
+                    keys.add(KEY_TAP_SCREEN);
+                    keys.add(KEY_DOUBLE_TAP_SCREEN);
+                    keys.add(KEY_PICK_UP);
+                    keys.add(KEY_PREVENT_RINGING);
+                    keys.add(KEY_GESTURE_GLOBAL_ACTIONS_PANEL); 
+           return keys;
+        }
+
+    };
 
 }
